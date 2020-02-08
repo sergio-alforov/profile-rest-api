eb89e7bdbc1ffb8e93893550bfe5f490fb48ba88
sergio-alforov <sergio.sergio.alforov@gmail.comgit config --global user.email sergio.sergio.alforov@gmail.com>
2020-02-08 14:12:44 +0200

    added hellow api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HellowApiView(APIView):
+    """Test API view"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            "Users HTTP methods as function (get, post, put, patch, delete)",
+            "Is similar to a traditional Django View",
+            "Gives you the most control over your application logic",
+            "Is mapped manually to URLs",
+        ]
+        return Response({'message': 'Hellow', 'an_apiview': an_apiview})
+
+
