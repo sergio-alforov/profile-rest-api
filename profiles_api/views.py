2236885cf050a3f6804825ba195025f304f9790c
sergio-alforov <sergio.sergio.alforov@gmail.comgit config --global user.email sergio.sergio.alforov@gmail.com>
2020-02-11 21:04:22 +0200

    added profile feed item

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import APISettings, api_settings
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -106,4 +106,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentications tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_pofile=self.request.user)
 
